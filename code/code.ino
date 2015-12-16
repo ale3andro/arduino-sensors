b66af95603b1ab57938c03a513a0cef29c4bb790
ale3andro <ale3andro@gmail.com>
2015-12-16 19:49:24 +0200

    Working SD Card, RTC and HD44780 LCD

--- code/code.ino
+++ code/code.ino
@@ -1,18 +1,57 @@
 #include <Wire.h>
 #include "RTClib.h"
+#include <SD.h>
+#include <LiquidCrystal.h>
+
  
 RTC_DS1307 RTC;
+Sd2Card card;
+SdVolume volume;
+SdFile root;
+LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
+
+
+const int chipSelect = 10;
  
 void setup () {
-    Serial.begin(57600);
-    Wire.begin();
-    RTC.begin();
- 
+  pinMode(10, OUTPUT);
+  Serial.begin(57600);
+  Wire.begin();
+  RTC.begin();
+  
   if (! RTC.isrunning()) {
     Serial.println("RTC is NOT running!");
     // following line sets the RTC to the date & time this sketch was compiled
     // uncomment it & upload to set the time, date and start run the RTC!
     //RTC.adjust(DateTime(__DATE__, __TIME__));
+  }
+  if (!card.init(SPI_HALF_SPEED, chipSelect)) {
+    Serial.println("initialization failed. Things to check:");
+    Serial.println("* is a card is inserted?");
+    Serial.println("* Is your wiring correct?");
+    Serial.println("* did you change the chipSelect pin to match your shield or module?");
+    return;
+  } else {
+   Serial.println("Wiring is correct and a card is present."); 
+  }
+  lcd.begin(16, 2);
+  // print the type of card
+  Serial.print("\nCard type: ");
+  switch(card.type()) {
+    case SD_CARD_TYPE_SD1:
+      Serial.println("SD1");
+      lcd.print("SD1");
+      break;
+    case SD_CARD_TYPE_SD2:
+      Serial.println("SD2");
+      lcd.print("SD2");
+      break;
+    case SD_CARD_TYPE_SDHC:
+      Serial.println("SDHC");
+      lcd.print("SDHC");
+      break;
+    default:
+      Serial.println("Unknown");
   }
  
 }
@@ -57,5 +96,8 @@ void loop () {
     Serial.println();
     
     Serial.println();
+    lcd.setCursor(0, 1);
+    // print the number of seconds since reset:
+    lcd.print(millis() / 1000);
     delay(3000);
 }
